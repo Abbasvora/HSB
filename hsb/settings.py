09a55a3a871259c13d02318f177b16b27c18a224
Abbasvora <abbasvora23@gmail.com>
2021-08-30 12:44:30 +0530

    static path added

--- hsb/settings.py
+++ hsb/settings.py
@@ -137,6 +137,7 @@ EMAIL_HOST_PASSWORD = 'wgplycwwjgunlryv'
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
